0c28387040c6da97d4a512fa22e86af0b77d0877
Yılmaz <hasanyilmazgursoy@gmail.com>
2024-10-24 17:36:10 +0300

    templates oluşturulup gerekli htmller yapıldı( base.html, index.html ve hakkimizda.html )

--- spor/spor_AI/views.py
+++ spor/spor_AI/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Sporcu
 
 # Create your views here.
 
 def anasayfa(request):
-    return render(request, 'index.html')
+    sporcu= Sporcu.objects.all()
+    return render(request, 'index.html',{'sporcu':sporcu})
 
 def hakkimizda(request):
     return render(request, 'hakkimizda.html')
