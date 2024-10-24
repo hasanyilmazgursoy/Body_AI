7eff0951be39627b41dc2c410208067f667c831b
Yılmaz <hasanyilmazgursoy@gmail.com>
2024-10-24 14:41:48 +0300

    templates oluşturulup gerekli htmller yapıldı( base.html, index.html ve hakkimizda.html )

--- spor/spor/urls.py
+++ spor/spor/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from spor_AI.views import anasayfa,hakkimizda
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', anasayfa),
+    path('hakkimizda/', hakkimizda),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- spor/spor_AI/views.py
+++ spor/spor_AI/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def anasayfa(request):
+    return render(request, 'index.html')
+
+def hakkimizda(request):
+    return render(request, 'hakkimizda.html')
